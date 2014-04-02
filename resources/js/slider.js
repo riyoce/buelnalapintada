a05e106c6cb03277fc2f075b640000e2f6687374
admin <admin@192.168.15.11>
2014-04-02 17:06:25 -0600

    iniciando

--- resources/js/slider.js
+++ resources/js/slider.js
@@ -19,36 +19,35 @@ function slider() {
     if (n >= x) {
         n = 0;
     }
-    
+
     if (n < 0) {
         n = 0;
     }
 
-    
-    if (z == 1) {        
+
+    if (z == 1) {
         array = new Array("img_1.jpg", "img_2.jpg", "img_3.jpg", "img_4.jpg");
         x = array.length;
     }
 
-    imageSlider.src = imagepath + array[n];          
+    imageSlider.src = imagepath + array[n];
     n++;
 }
 
 function nextImage() {
-    window.clearInterval(nIntervId);        
+    window.clearInterval(nIntervId);
     slider();
     nIntervId = window.setInterval(slider, "6000");
 }
 
 function prevImage() {
     window.clearInterval(nIntervId);
-    if (n == 1) {
-        n = x;   
-         n = n -1;
-    } else{
-        n = n -1;
+    if (n == 1 || n == 0) {
+        n = x;        
+    } else {
+        n = n - 1;
     }
-    
+
     slider();
     nIntervId = window.setInterval(slider, "6000");
 }
